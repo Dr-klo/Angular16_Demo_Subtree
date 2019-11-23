f9829321cd1e6d900fcd84a6fb063e621bcda73a
Dr.klo <kulinchik.leonid@gmail.com>
2019-11-23 19:19:49 +0300

    extend behaviour of product basket

--- src/CustomProductBasket.ts
+++ src/CustomProductBasket.ts
@@ -1,3 +1,12 @@
-export class CustomProductBasket{
+import {ProductBasket} from "@ModuleA/ProductBasket";
 
+export class CustomProductBasket extends ProductBasket{
+    public ToString(): string {
+        const products = this.getList();
+        let result = `Custom Product Basket(${products.length} items)`;
+        for(const product of products){
+            result+=`Product #${product.id} ${product.title}: ${product.count} items\n`;
+        }
+        return result;
+    }
 }
