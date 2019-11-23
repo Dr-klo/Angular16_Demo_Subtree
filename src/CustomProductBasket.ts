8af8d8f6b754c3225c7762c33b2b468b471d5d15
Dr.klo <kulinchik.leonid@gmail.com>
2019-11-23 19:21:24 +0300

    update basket behaviour

--- src/CustomProductBasket.ts
+++ src/CustomProductBasket.ts
@@ -2,9 +2,8 @@ import {ProductBasket} from "@ModuleA/ProductBasket";
 
 export class CustomProductBasket extends ProductBasket{
     public ToString(): string {
-        const products = this.getList();
-        let result = `Custom Product Basket(${products.length} items)`;
-        for(const product of products){
+        let result = `Custom Product Basket(${this.products.length} items)`;
+        for(const product of this.products){
             result+=`Product #${product.id} ${product.title}: ${product.count} items\n`;
         }
         return result;
