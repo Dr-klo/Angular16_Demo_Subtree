ad83a91182850d435e8bbb94f5b303bae9381cb7
Dr.klo <kulinchik.leonid@gmail.com>
2019-11-23 19:29:19 +0300

    Branch FeautreA behaviour update

--- src/CustomProductBasket.ts
+++ src/CustomProductBasket.ts
@@ -2,7 +2,7 @@ import {ProductBasket} from "@ModuleA/ProductBasket";
 
 export class CustomProductBasket extends ProductBasket{
     public ToString(): string {
-        let result = `Custom Product Basket(${this.products.length} items)`;
+        let result = super.ToString();
         for(const product of this.products){
             result+=`Product #${product.id} ${product.title}: ${product.count} items\n`;
         }
